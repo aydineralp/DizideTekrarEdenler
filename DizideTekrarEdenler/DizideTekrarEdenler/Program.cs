9e586d1f4714c310b85403c391aff5d364a83bc0
Veysel Aydın ERALP <veyselaydineralp@gmail.com>
2024-08-13 12:48:25 +0300

    DizideTekrarEdenler

---
+++ DizideTekrarEdenler/DizideTekrarEdenler/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DizideTekrarEdenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan dizi elemanlarını alıriz
+            Console.WriteLine("Diziyi girin :"); //bosluk kullan
+            string input = Console.ReadLine();
+            string[] elements = input.Split(' ');
+
+            // Elemanları ve tekrar sayılarını tutacak bir sözlük oluştururuz -dictionary
+            Dictionary<string, int> elementCounts = new Dictionary<string, int>();
+
+            // Elemanları say
+            foreach (string element in elements)
+            {
+                if (elementCounts.ContainsKey(element))
+                {
+                    elementCounts[element]++;
+                }
+                else
+                {
+                    elementCounts[element] = 1;
+                }
+            }
+
+            // Tekrar eden elemanları ve tekrar sayılarını yazdirir
+            Console.WriteLine("Tekrar eden elemanlar ve tekrar sayıları:");
+            foreach (KeyValuePair<string, int> kvp in elementCounts)
+            {
+                if (kvp.Value > 1)
+                {
+                    Console.WriteLine($"{kvp.Key}: {kvp.Value} kez");
+                }
+            }
+        }
+    }
+}
